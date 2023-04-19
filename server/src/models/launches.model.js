d46aa722c676199cecbe7e6e7903f914f71aa4e7
leeviallu <68004427+leeviallu@users.noreply.github.com>
2023-04-19 19:40:45 +0300

    Added mongoose schema modules

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,3 +1,5 @@
+// const launches = require('./launches.mongo');
+
 const launches = new Map();
 
 let latestFlightNumber = 100;
@@ -8,7 +10,7 @@ const launch = {
     rocket: 'Explorer IX1',
     launchDate: new Date('December 27, 2030'),
     target: 'Kepler-442 b',
-    customer: ['ZTM', 'NASA'],
+    customers: ['ZTM', 'NASA'],
     upcoming: true,
     success: true, 
 }
